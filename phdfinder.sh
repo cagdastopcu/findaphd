0133f4645a9c64af66062e31f4dfceb94ae2452a
Cagdas Topcu <topcucagdas@gmail.com>
2017-04-06 11:20:49 +0200

    Update phdfinder.sh

--- phdfinder.sh
+++ phdfinder.sh
@@ -2,7 +2,7 @@
 #  @author: Cagdas Topcu, MSc.
 #  06 April 2017
 # don't panic you will find a good PhD position.
-# you shoul use it with cron
+# you should use it with cron
 # example: crontab -e and add this line: 0,30 * * * * /home/username/phdfinder/phdfinder
 # just change your e-mail address
 newdata=$(curl -L https://euraxess.ec.europa.eu/jobs/search | grep -Po '(?<=href=")[^<]*' | tr '[A-Z]' '[a-z]' | grep -e phd -e p.h.d -e ph.d -e p.hd -e doctorate -e doctoral | awk '$0="https:/euraxess.ec.europa.eu/"$0')
