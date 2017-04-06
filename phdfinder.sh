02bcc5803537f8bdfe794b9e1789c30d94627ef3
Cagdas Topcu <topcucagdas@gmail.com>
2017-04-06 02:59:04 +0200

    Add files via upload
    This is the main script

---
+++ phdfinder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#  @author: Cagdas Topcu, MSc.
+#  06 April 2017
+# don't panic you will find a good PhD position.
+# you should use this code with watch command.
+# just change your e-mail address
+text=$(curl -L https://euraxess.ec.europa.eu/jobs/search | grep -Po '(?<=href=")[^<]*' | tr '[A-Z]' '[a-z]' | grep -e phd -e p.h.d -e ph.d -e p.hd -e doctorate -e doctoral | awk '$0="https:/euraxess.ec.europa.eu/"$0')
+
+mail -s "Hell yeah!" "youremail@email.com" <<EOF
+$text
+EOF
