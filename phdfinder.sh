660539ddf730b489bd0efe8f833a5b478f23a325
Cagdas Topcu <topcucagdas@gmail.com>
2017-04-06 11:04:04 +0200

    now it checks old and new jobs

--- phdfinder.sh
+++ phdfinder.sh
@@ -2,11 +2,21 @@
 #  @author: Cagdas Topcu, MSc.
 #  06 April 2017
 # don't panic you will find a good PhD position.
-# you should use this code with cron command.
+# you shoul use it with cron
 # example: crontab -e and add this line: 0,30 * * * * /home/username/phdfinder/phdfinder
 # just change your e-mail address
-text=$(curl -L https://euraxess.ec.europa.eu/jobs/search | grep -Po '(?<=href=")[^<]*' | tr '[A-Z]' '[a-z]' | grep -e phd -e p.h.d -e ph.d -e p.hd -e doctorate -e doctoral | awk '$0="https:/euraxess.ec.europa.eu/"$0')
+newdata=$(curl -L https://euraxess.ec.europa.eu/jobs/search | grep -Po '(?<=href=")[^<]*' | tr '[A-Z]' '[a-z]' | grep -e phd -e p.h.d -e ph.d -e p.hd -e doctorate -e doctoral | awk '$0="https:/euraxess.ec.europa.eu/"$0')
 
-mail -s "Hell yeah!" "youremail@email.com" <<EOF
-$text
+# check the pseudo-db file if it does not exist create one
+[ -e "pseudodbforphdf" ] || touch "pseudodbforphdf"
+# save new data
+echo $newdata > newpseudodbforphdf
+# compare old and new ps-db file
+if ! (cmp -s newpseudodbforphdf pseudodbforphdf);then
+	# change old file
+echo $newdata > pseudodbforphdf
+	# send the report
+mail -s "Hey check new PhD positions!" "myemail@myemail.com" <<EOF
+$newdata
 EOF
+fi
